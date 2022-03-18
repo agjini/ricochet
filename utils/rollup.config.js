1876da35199ffa50f651c128afc7fae0521e1a50
Augustin Gjini <augustin.gjini@gmail.com>
2022-03-18 16:14:04 +0100

    wip

--- utils/rollup.config.js
+++ utils/rollup.config.js
@@ -1,41 +1,70 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
 
 export default [
     {
         input: "src/api/index.ts",
-        output: {
-            file: 'dist/api.js',
-            format: 'umd',
-            name: 'api'
-        },
+        output: [
+            {
+                file: "dist/api.js",
+                format: "cjs",
+                exports: "named",
+                sourcemap: true
+            },
+            {
+                file: "dist/api.esm.js",
+                format: "esm",
+                sourcemap: true
+            }
+        ],
         plugins: [
             peerDepsExternal(),
+            terser(),
             typescript()
         ],
         external: []
     },
     {
         input: "src/client/index.ts",
-        output: {
-            file: 'dist/client.js',
-            format: 'umd',
-            name: 'client'
-        },
+        output: [
+            {
+                file: "dist/client.js",
+                format: "cjs",
+                exports: "named",
+                sourcemap: true
+            },
+            {
+                file: "dist/client.esm.js",
+                format: "esm",
+                sourcemap: true
+            }
+        ],
         plugins: [
             peerDepsExternal(),
+            terser(),
             typescript()
         ],
         external: []
     },
     {
         input: "src/service/index.ts",
-        output: {
-            file: 'dist/service.js',
-            format: 'cjs'
-        },
+        output: [
+            {
+                file: "dist/service.js",
+                format: "cjs",
+                exports: "named",
+                sourcemap: true
+            },
+            {
+                file: "dist/service.esm.js",
+                format: "esm",
+                sourcemap: true
+            }
+        ],
         plugins: [
             peerDepsExternal(),
+            terser(),
             typescript()
         ],
         external: ["multer", "mongodb", "nodemailer", "bson", "bcrypt", "jsonwebtoken", "crypto", "next"]
